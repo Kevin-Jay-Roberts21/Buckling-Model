43922a934c9dc20dd259c6879c8bfe03f77ec678
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2026-01-19 14:59:53 -0700

    Added stability check.

--- radially_symmetric_solution_two_region.py
+++ radially_symmetric_solution_two_region.py
@@ -9,7 +9,9 @@ from scipy.integrate import solve_bvp
 from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 
-# Defining the material parameters
+#################################################
+# Defining the material parameters and geometry #
+#################################################
 R_v = 50 # void radius (um)
 R_s = 150 # subcortex radius (um)
 R_c = 200 # cortex radius (um)
@@ -22,7 +24,7 @@ mu_c = 34200 # shear modulus in cortex region (Pa)
 g_r_s = 1.05 # growth multiplier in the radial direction in subcortex region (#)
 g_r_c = 1.1 # growth multiplier in the radial direction in cortex region (#)
 g_theta_s = 1.05 # growth multiplier in the circumferential direction in subcortex region (#)
-g_theta_c = 1.5 # growth multiplier in the circumferential direction in cortex region (#)
+g_theta_c = 3.1 # growth multiplier in the circumferential direction in cortex region (#)
 g_z = 1 # growth multiplier in the axial direction (#)
 
 C_z = 1 # principal stretch in the axial direction (#)
@@ -30,24 +32,26 @@ P_f = 19.62 # external pressure (hydrostatic pressure) (Pa)
 P_in = 0 # pressure inward normal to R_v boundary (Pa)
 P_out = 0 # pressure inward normal to the R_c boundary (Pa)
 
-# putting values for cortex and subcortex in a dictionary
-subcortex_vals = dict(lambd=lambda_s, mu=mu_s, g_r=g_r_s, g_theta=g_theta_s, g_z=g_z)
-cortex_vals = dict(lambd=lambda_c, mu=mu_c, g_r=g_r_c, g_theta=g_theta_c, g_z=g_z)
+# putting values for cortex and subcortex in a dictionary, used for different regions
+subcortex_vals = dict(lam=lambda_s, mu=mu_s, g_r=g_r_s, g_theta=g_theta_s)
+cortex_vals = dict(lam=lambda_c, mu=mu_c, g_r=g_r_c, g_theta=g_theta_c)
 
-# For deciding where or not the inner boundary is fixed:
-# "fixed" for fixed boundary
-# "pressure" for pressure P_f applied to boundary
+# For deciding where or not the inner subcortex boundary is fixed:
+# "fixed" -> fixed boundary
+# "pressure" -> pressure P_f applied normal to boundary
 INNER_BC = "fixed"
 
-# Computing the stresses and natural log terms in terms of R, r, and r_prime
+
+#############################
+# Defining stresses and eta #
+#############################
 def stresses(R, r, r_prime, params):
 
     # getting the parameters
-    lambd = params['lambd']
+    lam = params['lam']
     mu = params['mu']
     g_r = params['g_r']
     g_theta = params['g_theta']
-    g_z = params['g_z']
 
     # avoiding division problems
     R = np.maximum(R, 1e-30)
@@ -56,19 +60,21 @@ def stresses(R, r, r_prime, params):
 
     # computing the ln term and using it in the eta term
     ln = np.log((r_prime*(r/R)*C_z) / (g_r*g_theta*g_z))
-    eta = lambd * ln - mu
+    eta = lam * ln - mu
 
     P_RR = mu*(r_prime/(g_r**2)) + eta/r_prime
     P_ThetaTheta = mu*((r/R)/(g_theta**2)) + eta*(R/r)
 
-    return P_RR, P_ThetaTheta, ln, eta
+    return P_RR, P_ThetaTheta, eta
 
-# Computing the partial derivatives in the equilibrium radial equation. They are needed from the chain rule since:
-# ∂P_RR/∂R = ∂P_RR/∂R + (∂P_RR/∂r)*y0' + (∂P_RR/∂r')*y1', where y0 = r and y1 = r'
-def partial_derivs(R, r, r_prime, ln, eta, params):
+################################
+# Computing the partial derivs #
+################################
+# NOTE: ∂P_RR/∂R = ∂P_RR/∂R + (∂P_RR/∂r)*y0' + (∂P_RR/∂r')*y1', where y0 = r and y1 = r'
+def partial_derivs(R, r, r_prime, eta, params):
 
     # getting the params
-    lambd = params['lambd']
+    lam = params['lam']
     mu = params['mu']
     g_r = params['g_r']
 
@@ -78,20 +84,19 @@ def partial_derivs(R, r, r_prime, ln, eta, params):
     r_prime = np.maximum(r_prime, 1e-30)
 
     # ∂P_RR/∂r'
-    dP_dr_prime = (mu/(g_r**2)) + (lambd-eta)/(r_prime**2)
+    dP_dr_prime = (mu/(g_r**2)) + (lam-eta)/(r_prime**2)
 
     # ∂P_RR/∂r
-    dP_dr = lambd/(r_prime*r)
+    dP_dr = lam/(r_prime*r)
 
     # ∂P_RR/∂R
-    dP_dR = -lambd/(r_prime*R)
+    dP_dR = -lam/(r_prime*R)
 
     return dP_dR, dP_dr, dP_dr_prime
 
-# Defining the ODE system for the solve_bvp solver and defining a "make" ode_system
-# this has to be done inside the function since params is piecewise. We don't need
-# this design in the one region case
-
+#############################################################
+# Defining the ODE function object for the solve_bvp solver #
+#############################################################
 def make_ode_system(params):
     def ode_system(R, y):
 
@@ -106,11 +111,11 @@ def make_ode_system(params):
         r = y[0]
         r_prime = y[1]
 
-        P_RR, P_ThetaTheta, ln, eta = stresses(R, r, r_prime, params)
-        dP_dR, dP_dr, dP_dr_prime = partial_derivs(R, r, r_prime, ln, eta, params)
+        P_RR, P_ThetaTheta, eta = stresses(R, r, r_prime, params)
+        dP_dR, dP_dr, dP_dr_prime = partial_derivs(R, r, r_prime, eta, params)
 
-        # dP_RR/dR + (P_RR - P_ThetaTheta)/R = 0 -->
-        # ∂P/∂R + (∂P/∂r)*r' + (∂P/∂r')*r'' + (P_RR - P_TT)/R = 0 -->
+        # dP_RR/dR + (P_RR - P_ThetaTheta)/R = 0 ->
+        # ∂P/∂R + (∂P/∂r)*r' + (∂P/∂r')*r'' + (P_RR - P_TT)/R = 0 ->
         # (∂P/∂r')*r'' = -(P_RR - P_TT)/R - ∂P/∂R - (∂P/∂r)*r'
         rhs = -(P_RR - P_ThetaTheta)/R - dP_dR - dP_dr*r_prime
 
@@ -120,19 +125,24 @@ def make_ode_system(params):
         return np.vstack((r_prime, r_double_prime))
     return ode_system
 
-# We need BVP solvers for each region given an interface displacement, which is defined as r_s
+# We need BVP solvers for each region given an interface displacement.
+# We define the interface displacement as r_s
 
-# BVP solver for the subcortex given r_s
+##########################################
+# BVP solver for the subcortex given r_s #
+##########################################
 def solve_subcortex(r_s):
     ode = make_ode_system(subcortex_vals)
 
     R_mesh = np.linspace(R_v, R_s, 200)
     y_guess = np.vstack((np.linspace(R_v, r_s, R_mesh.size), np.ones_like(R_mesh)))
 
-    # similar to bc function in "one region" code with modification for fixed and not fixed inner boundary
+    # defining the boundary conditions for subcortex
     def bc(ya, yb):
-        r_a, rp_a = ya
-        r_b, rp_b = yb
+        r_a = ya[0]
+        rp_a = ya[1]
+        r_b = yb[0]
+        # rp_b = yb[1] # not used
 
         # boundary condition at the interface
         bc_int = r_b - r_s
@@ -141,7 +151,7 @@ def solve_subcortex(r_s):
             bc_inner = r_a - R_v
 
         elif INNER_BC == "pressure":
-            P_RR_a, _, _, _ = stresses(np.array([R_v]), np.array([r_a]), np.array([rp_a]), subcortex_vals)
+            P_RR_a, _, _ = stresses(np.array([R_v]), np.array([r_a]), np.array([rp_a]), subcortex_vals)
             P_RR_a = P_RR_a[0]
 
             bc_inner = P_RR_a - P_in*C_z*(r_a/R_v)
@@ -154,7 +164,9 @@ def solve_subcortex(r_s):
     sol = solve_bvp(ode, bc, R_mesh, y_guess, max_nodes=5000, tol=1e-6)
     return sol
 
-# BVP solver for the subcortex given r_s
+##########################################
+# BVP solver for the subcortex given r_s #
+##########################################
 def solve_cortex(r_s):
     ode = make_ode_system(cortex_vals)
 
@@ -162,13 +174,15 @@ def solve_cortex(r_s):
     y_guess = np.vstack((np.linspace(r_s, R_c, R_mesh.size), np.ones_like(R_mesh)))
 
     def bc(ya, yb):
-        r_a, rp_a = ya
-        r_b, rp_b = yb
+        r_a = ya[0]
+        # rp_a = ya[1] # not used
+        r_b = yb[0]
+        rp_b = yb[1]
 
         # Boundary condition at the interface
         bc_int = r_a - r_s
 
-        P_RR_b, _, _, _ = stresses(np.array([R_c]), np.array([r_b]), np.array([rp_b]), cortex_vals)
+        P_RR_b, _, _ = stresses(np.array([R_c]), np.array([r_b]), np.array([rp_b]), cortex_vals)
         P_RR_b = P_RR_b[0]
 
         bc_outer = P_RR_b + P_out*C_z*(r_b/R_c)
@@ -178,21 +192,24 @@ def solve_cortex(r_s):
     sol = solve_bvp(ode, bc, R_mesh, y_guess, max_nodes=5000, tol=1e-6)
     return sol
 
-# Adding a stress jump function for rooting finding of P_RR^-(R_s) - P_RR^+(R_s)
+########################################################################
+# Stress jump function for root finding: P_RR^-(R_s) - P_RR^+(R_s) = 0 #
+########################################################################
 def stress_jump(r_s):
     sol1 = solve_subcortex(r_s)
     sol2 = solve_cortex(r_s)
 
-    # is neither bvp succeeded, then return a big number to avoid bad guesses
+    # if one of bvp didn't succeed, then return a big number to avoid bad guesses
     if (not sol1.success) or (not sol2.success):
+        print("One or both of the bvp did not succeed.")
         return np.sign(r_s - R_s)*1e9
 
     # Evaluate each side at the interface
     r1_s, rp1_s = sol1.sol(np.array([R_s]))
     r2_s, rp2_s = sol2.sol(np.array([R_s]))
 
-    P_RR_1, _, _, _ = stresses(np.array([R_s]), np.array([r1_s[0]]), np.array([rp1_s[0]]), subcortex_vals)
-    P_RR_2, _, _, _ = stresses(np.array([R_s]), np.array([r2_s[0]]), np.array([rp2_s[0]]), cortex_vals)
+    P_RR_1, _, _ = stresses(np.array([R_s]), np.array([r1_s[0]]), np.array([rp1_s[0]]), subcortex_vals)
+    P_RR_2, _, _ = stresses(np.array([R_s]), np.array([r2_s[0]]), np.array([rp2_s[0]]), cortex_vals)
 
     return P_RR_1[0] - P_RR_2[0]
 
@@ -222,11 +239,11 @@ r_s_star = root.root
 print("Interface displacement r(R_s) =", r_s_star)
 
 # Solve one last time with the matched interface displacement
-sol_sub = solve_subcortex(r_s_star)
-sol_ctx = solve_cortex(r_s_star)
+cortex_solution = solve_cortex(r_s_star)
+subcortex_solution = solve_subcortex(r_s_star)
 
-print("Subcortex converged:", sol_sub.success, sol_sub.message)
-print("Cortex converged:   ", sol_ctx.success, sol_ctx.message)
+print("Subcortex converged:", cortex_solution.success, cortex_solution.message)
+print("Cortex converged:   ", subcortex_solution.success, subcortex_solution.message)
 
 # -----------------------------
 # Stitch solutions for plotting
@@ -235,8 +252,8 @@ R1 = np.linspace(R_v, R_s, 300)
 R2 = np.linspace(R_s, R_c, 300)
 
 # These are 1D arrays already (length 300)
-r1, rp1 = sol_sub.sol(R1)
-r2, rp2 = sol_ctx.sol(R2)
+r1, rp1 = cortex_solution.sol(R1)
+r2, rp2 = subcortex_solution.sol(R2)
 
 # Stitch (skip the duplicate interface point in region 2)
 R_all  = np.concatenate([R1, R2[1:]])
@@ -244,11 +261,39 @@ r_all  = np.concatenate([r1, r2[1:]])
 rp_all = np.concatenate([rp1, rp2[1:]])
 
 # Stresses (pass 1D arrays)
-P_RR_1, P_TT_1, _, _ = stresses(R1, r1, rp1, subcortex_vals)
-P_RR_2, P_TT_2, _, _ = stresses(R2, r2, rp2, cortex_vals)
+P_RR_1, P_TT_1, _ = stresses(R1, r1, rp1, subcortex_vals)
+P_RR_2, P_TT_2, _ = stresses(R2, r2, rp2, cortex_vals)
 
 P_RR_all = np.concatenate([P_RR_1, P_RR_2[1:]])
 P_TT_all = np.concatenate([P_TT_1, P_TT_2[1:]])
+
+
+#######################################################
+# Checking for Instability: if lam_r/lam_theta >= 2.4 #
+#######################################################
+def instability_check(cortex_solution, subcortex_solution):
+    # check at the outer surface R = R_c
+    y = cortex_solution.sol(np.array([R_c]))
+    r_out = float(y[0][0])
+    rp_out = float(y[1][0])
+
+    # computing the total stretches
+    F_RR = rp_out
+    F_ThetaTheta = r_out/R_c
+
+    # elastic stretches
+    g_r = cortex_vals['g_r']
+    g_theta = cortex_vals['g_theta']
+
+    # lam_r and lam_theta are the ELASTIC principle stretches
+    lam_r = F_RR / g_r
+    lam_theta = F_ThetaTheta / g_theta
+
+    ratio = lam_r / lam_theta
+
+    print(f"Instability ratio: {ratio}")
+
+instability_check(cortex_solution, subcortex_solution)
 
 
 
@@ -294,9 +339,9 @@ def plot_reference_vs_deformed_boundaries_two_region(
     plt.show()
 
 # inner, interface and outer deformed radii
-r_in  = sol_sub.sol(np.array([R_v]))[0][0]
+r_in  = cortex_solution.sol(np.array([R_v]))[0][0]
 r_s   = r_s_star
-r_out = sol_ctx.sol(np.array([R_c]))[0][0]
+r_out = subcortex_solution.sol(np.array([R_c]))[0][0]
 
 plot_reference_vs_deformed_boundaries_two_region(r_in, r_s, r_out)
 
